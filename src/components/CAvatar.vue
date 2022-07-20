3f77b0a92d3bbc0aea086615ae852af5a4eb7171
castetus <kboikov@mail.ru>
2022-07-20 12:35:51 +0300

    add button component

---
+++ src/components/CAvatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="c-avatar">
+    <q-avatar :size="size">
+      <img :src="link">
+    </q-avatar>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CAvatar',
+  props: {
+    size: {
+      type: String,
+      default: (): string => 'sm',
+    },
+    link: {
+      type: String,
+      default: (): string => '',
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="c-button">
+    <slot name="prepend"></slot>
+      <span class="c-button__text">{{text}}</span>
+    <slot name="append"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CButton',
+  props: {
+    size: {
+      type: String,
+      default: (): string => 'md',
+    },
+    bgColor: {
+      type: String,
+      default: (): string => '',
+    },
+    iconSize: {
+      type: String,
+      default: (): string => '',
+    },
+    iconName: {
+      type: String,
+      default: (): string => '',
+    },
+    text: {
+      type: String,
+      default: (): string => '',
+    },
+    textColor: {
+      type: String,
+      default: (): string => '#6E7191',
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+  .c-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: fit-content;
+    background: v-bind(bgColor);
+    color: v-bind(textColor);
+    border-radius: 4px;
+    padding: 7px 12px;
+    cursor: pointer;
+    &__text {
+    }
+    // &:hover {
+
+    // }
+  }
+</style>
